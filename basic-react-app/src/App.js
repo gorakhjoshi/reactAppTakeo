161402962a0f503734b091bd5dd30c02dcdda383
Gorakh Raj joshi <gorakh626@gmail.com>
2022-07-01 07:53:10 +0545

    final app build

--- basic-react-app/src/App.js
+++ basic-react-app/src/App.js
@@ -1,4 +1,3 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
 import FeedbakStats from "./components/FeedbakStats";
@@ -8,26 +7,26 @@ import { FeedbackProvider } from "./context/FeedbackContext";
 function App() {
   return (
     <FeedbackProvider>
-      <BrowserRouter>
-        <div className="container">
-          <Header text="Header" />
-          <Routes>
-            <Route
+      {/* <BrowserRouter> */}
+      <div className="container">
+        <Header text="Header" />
+        {/* <Routes> */}
+        {/* <Route
               path="/"
               element={
-                <>
-                  <FeedbackForm />
-                  <FeedbackList />
-                  <FeedbakStats />
-                </>
+                <> */}
+        <FeedbackForm />
+        <FeedbackList />
+        <FeedbakStats />
+        {/* </>
               }
-            />
+            /> */}
 
-            <Route path="about" element={<div>I am from about</div>} />
-            <Route path="*" element={<div>404 Not Found</div>} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+        {/* <Route path="about" element={<div>I am from about</div>} /> */}
+        {/* <Route path="*" element={<div>404 Not Found</div>} /> */}
+        {/* </Routes> */}
+      </div>
+      {/* </BrowserRouter> */}
     </FeedbackProvider>
   );
 }
