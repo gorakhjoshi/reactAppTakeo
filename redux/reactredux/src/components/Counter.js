1b8e85d61885eeacedc10179a0105252980b9314
Gorakh Raj joshi <gorakh626@gmail.com>
2022-07-12 06:17:55 +0545

    state consumed from component

--- redux/reactredux/src/components/Counter.js
+++ redux/reactredux/src/components/Counter.js
@@ -1,12 +1,15 @@
 import classes from "./Counter.module.css";
+import { useSelector } from "react-redux";
 
 const Counter = () => {
+  const count = useSelector((state) => state.count);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>-- {count} --</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
