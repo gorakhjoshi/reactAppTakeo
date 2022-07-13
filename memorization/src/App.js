f94989175cdaef5fef38198f33f842e76acd82c8
Gorakh Raj joshi <gorakh626@gmail.com>
2022-07-13 06:41:46 +0545

    memorization broken

--- memorization/src/App.js
+++ memorization/src/App.js
@@ -1,10 +1,12 @@
 import { useState, memo } from "react";
 import "./App.css";
 
-function Swatch({ color }) {
+function Swatch({ params }) {
   console.log("Swatch is rendering");
   return (
-    <div style={{ width: 100, height: 100, backgroundColor: color }}></div>
+    <div
+      style={{ width: 100, height: 100, backgroundColor: params.color }}
+    ></div>
   );
 }
 
@@ -13,12 +15,16 @@ const MemorizedComponent = memo(Swatch);
 function App() {
   console.log("App is rendering");
   const [renderApp, setRenderApp] = useState(0);
+  const [color, setColor] = useState("red");
   console.log(`Current value of renderApp: ${renderApp}`);
 
   return (
     <div className="app">
       <button onClick={() => setRenderApp(renderApp + 1)}>Re-render App</button>
-      <MemorizedComponent color="red" />
+      <button onClick={() => setColor(color === "red" ? "green" : "red")}>
+        Change color
+      </button>
+      <MemorizedComponent params={{ color }} />
     </div>
   );
 }
