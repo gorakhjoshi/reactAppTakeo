b85bc52225d06fb95ebf0cd9b52f626838e7d1b8
Gorakh Raj joshi <gorakh626@gmail.com>
2022-07-15 07:54:55 +0545

    typing popular prop type

--- typescript/react-typescript/src/App.tsx
+++ typescript/react-typescript/src/App.tsx
@@ -1,9 +1,57 @@
-function NameTag({ name }: { name: string }) {
-  return <div>{name}</div>;
+type ProjectOne = {
+  frontend: string;
+  backend: string;
+};
+
+type About = {
+  backend: string[];
+  frontend: string[];
+  client: number;
+  projectOne: ProjectOne;
+};
+
+type NameProp = {
+  name: string;
+  popularity?: number;
+  isDisabled: boolean;
+  status: "loading" | "error" | "success";
+  favLanguages: string[];
+  about: About;
+  // keywords: [key: number] : string
+  handleChange: () => void;
+};
+
+function NameTag({ name, popularity = 10000, isDisabled, status }: NameProp) {
+  return (
+    <>
+      <div>{status}</div>
+      <h1>{name}</h1>
+      <h2>{popularity}</h2>
+      <button disabled={isDisabled}>Click Me</button>
+    </>
+  );
 }
 
 function App() {
-  return <NameTag name="React" />;
+  return (
+    <NameTag
+      name="React"
+      popularity={39500}
+      isDisabled={true}
+      status="loading"
+      favLanguages={["python", "java", "go", "rust", "nodejs"]}
+      about={{
+        backend: ["Java", "PHP", "Python"],
+        frontend: ["React", "Angular", "vuejs"],
+        client: 10,
+        projectOne: { frontend: "React", backend: "Java with Springboot" },
+      }}
+      // keywords={{ 1: "react", 2: "angular" }}
+      // keywords={{ one: "react", two: "angular" }}
+      // onClick = {() => {}}
+      handleChange={() => {}}
+    />
+  );
 }
 
 export default App;
---
+++ typescript/react-typescript/src/type/propType.ts
@@ -0,0 +1,5 @@
+export type NameProp = {
+  name: string;
+  popularity?: number;
+  isDisabled: boolean
+};
