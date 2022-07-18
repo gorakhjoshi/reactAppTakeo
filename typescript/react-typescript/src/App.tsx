ff55fd62240969f86d457eedd59738d309ee1c70
Gorakh Raj joshi <gorakh626@gmail.com>
2022-07-18 06:32:06 +0545

    typed CSS

--- typescript/react-typescript/src/App.tsx
+++ typescript/react-typescript/src/App.tsx
@@ -1,13 +1,15 @@
-type NameTagChildren = { children: React.ReactNode };
+type NameTagChildren = {
+  children: React.ReactNode;
+  style: React.CSSProperties;
+};
 
-function NameTag({ children }: NameTagChildren) {
-  return <>{children}</>;
+function NameTag({ children, style }: NameTagChildren) {
+  return <div style={style}>{children}</div>;
 }
 
 function App() {
   return (
-    <NameTag>
-      <h1>I love React</h1>
+    <NameTag style={{ backgroundColor: "red" }}>
       <h1>I love React</h1>
     </NameTag>
   );
