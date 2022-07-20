471bb1844610d35c0a6b4cb6d53a86d7414d3baa
Gorakh Raj joshi <gorakh626@gmail.com>
2022-07-20 07:35:57 +0545

    typing events

--- typescript/use-state-no-default/src/App.tsx
+++ typescript/use-state-no-default/src/App.tsx
@@ -1,13 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-export default function App() {
+function App() {
   const [count, setCount] = useState(0);
+  console.log(count)
+  function clickHandler(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    console.log(e);
+  }
 
-  console.log(count);
+  function changeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    setCount(+e.target.value);
+  }
 
-  useEffect(() => {
-    setTimeout(() => setCount(count + 1), 1000);
-  }, []);
-
-  return <div>{count}</div>;
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" onChange={changeHandler} />
+      <button onClick={clickHandler}>Submit</button>
+    </form>
+  );
 }
+
+export default App;
