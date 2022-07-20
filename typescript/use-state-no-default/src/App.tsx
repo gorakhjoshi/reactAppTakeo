b891b1bba524a585cdf78eb386dd3f8c27654c16
Gorakh Raj joshi <gorakh626@gmail.com>
2022-07-20 06:57:51 +0545

    typing usestate with no default value

--- typescript/use-state-no-default/src/App.tsx
+++ typescript/use-state-no-default/src/App.tsx
@@ -1,7 +1,37 @@
-import shuffle from "lodash.shuffle";
+import { fetchCharacter } from "./character";
+import { useEffect, useState } from "react";
+import { CharacterType } from "./character";
+import Loading from "./Loading";
 
 function App() {
-  return <div>App</div>;
+  const [character, setCharacter] = useState<null | CharacterType>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchCharacter().then((response) => {
+        setCharacter(response);
+        setLoading(false);
+      });
+    }, 1000);
+  }, []);
+
+  // if (loading) {
+  //   return <Loading />;
+  // } else {
+  //   return <div>{character.name}</div>;
+  // }
+
+  // if (!character) {
+  //   return <Loading />;
+  // } else {
+  //   return <div>{character.name}</div>;
+  // }
+
+  // return !character ? <Loading /> : <div>{character.name}</div>;
+
+  // return character && <div>{character.name}</div>;
+  return <div>{character?.name}</div>;
 }
 
 export default App;
