4716c12d79a8990edd7d94bd578e121d468f6ab0
Gorakh Raj joshi <gorakh626@gmail.com>
2022-07-21 06:30:47 +0545

    useReducer initial

--- typescript/typescript-reducer/src/App.tsx
+++ typescript/typescript-reducer/src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+
+const initialState = {
+  numberOfPeople: 10,
+  slicesPerPerson: 2,
+  slicePerPie: 0,
+  pizzaNeeded: 0,
+};
+
+function reducer(state: any, action: any) {
+  return state;
+}
+
+function Calculation({ count }: { count: any }) {
+  return (
+    <div className="calculation">
+      <p className="caption">Number of pizzas Needed</p>
+      <p className="count">{count}</p>
+    </div>
+  );
+}
+
+function Calculator({ state }: { state: any }) {
+  return (
+    <div>
+      <form>
+        <label htmlFor="number-of-people">Number of People</label>
+        <input type="text" id="number-of-people" value={state.numberOfPeople} />
+        <label htmlFor="slices-per-person">Slices Per Person</label>
+        <input
+          type="text"
+          id="slices-per-person"
+          value={state.slicesPerPerson}
+        />
+        <label htmlFor="slices-per-pie">Slices Per Pie</label>
+        <input type="text" id="slices-per-pie" value={state.slicePerPie} />
+      </form>
+    </div>
+  );
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Calculation count={state.pizzaNeeded} />
+      <Calculator state={state} />
     </div>
   );
 }
