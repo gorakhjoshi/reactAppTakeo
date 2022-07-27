ada494b959611cee4c54c92c16da49e0f79dd091
Gorakh Raj joshi <gorakh626@gmail.com>
2022-07-27 06:13:11 +0545

    absolute URL

---
+++ prettier-eslint-husky-jsconfig/src/views/front/main/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+// Abolute Import
+// import List from "../../../components/List";
+// Relative import
+
+import List from "components/List";
+
+function Home() {
+  return (
+    <div>
+      <List />
+    </div>
+  );
+}
+
+export default Home;
