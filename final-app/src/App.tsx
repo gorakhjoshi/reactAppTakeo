64e8f3d057f1051fd1ff37e7abb71c5a2ca3df73
Gorakh Raj joshi <gorakh626@gmail.com>
2022-08-01 07:03:54 +0545

    eslint and prettier setup

--- final-app/src/App.tsx
+++ final-app/src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div>App</div>;
-}
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <Stack spacing={2} direction="row">
+              <Button variant="contained">React TypeScript App</Button>
+            </Stack>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
